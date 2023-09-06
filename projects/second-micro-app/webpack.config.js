3bb18baa4a729df244aa0af69632b34077f0aafe
itsmehrd <kingharish22@gmail.com>
2023-09-06 07:51:05 +0530

    Module federation added - SECOND MICROAPP

---
+++ projects/second-micro-app/webpack.config.js
@@ -0,0 +1,54 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "secondMicroApp",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      
+        // For remotes (please adjust)
+        // name: "secondMicroApp",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './projects/second-micro-app/src/app/app.component.ts',
+        // },        
+        
+        // For hosts (please adjust)
+        // remotes: {
+        //     "containerApp": "containerApp@http://localhost:6543/remoteEntry.js",
+        //     "firstMicroApp": "firstMicroApp@http://localhost:5000/remoteEntry.js",
+        //     "thirdMicroApp": "thirdMicroApp@http://localhost:4200/remoteEntry.js",
+
+        // },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
